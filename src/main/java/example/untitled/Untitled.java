2f67db2f11311986d957d0632f2657623ffbd814
olzh1 <oljas.aitmanov@gmail.com>
2023-10-08 21:57:52 +0600

    a

--- src/main/java/example/untitled/Untitled.java
+++ src/main/java/example/untitled/Untitled.java
@@ -1,21 +1,26 @@
 
 package example.untitled;
 
-import java.util.logging.Logger;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Untitled extends JavaPlugin {
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class Untitled extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Logger nm = getLogger();
-        nm.info("hello");
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
-    
 
-    @Override
-    public void onDisable() {
-
+    @EventHandler
+    public void onPlayerJump(@NotNull PlayerMoveEvent event) {
+        if (event.getFrom().getY() < event.getTo().getY()) {
+            event.getPlayer().sendMessage( "\u041c\u043e\u043b\u043e\u0434\u0435\u0446!");
+        }
     }
+
 }
